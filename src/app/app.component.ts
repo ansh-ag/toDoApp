c55d7ad1d57332ef954e71a965f446a7d288fd81
Anshul R <47388377+anshulrdamco@users.noreply.github.com>
2021-05-16 12:10:51 +0530

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'toDoApp';
+
+  myToDoList = [];
+
+  constructor(){
+    const myList = JSON.parse(localStorage.getItem('cacheList'));
+
+    if(myList !== null){
+      this.myToDoList = myList;
+    }
+
+  }
+
+  addNew(value : string){
+    let myToDo = {
+      "text" : value,
+      "isCompleted" : false
+    }
+
+    this.myToDoList.push(myToDo);
+
+    localStorage.setItem('cacheList', JSON.stringify(this.myToDoList));
+  }
+
+  done(index:number){
+   this.myToDoList[index].isCompleted = true; 
+   localStorage.setItem('cacheList', JSON.stringify(this.myToDoList));
+
+  }
+  
+
+  remove(index:number){
+    this.myToDoList.splice(index,1);  
+    localStorage.setItem('cacheList', JSON.stringify(this.myToDoList));
+
+  }
+
+
 }
